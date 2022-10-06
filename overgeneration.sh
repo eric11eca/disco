c2de8f58c35db7c9996bdfece6e61cd0d54b2b6e
root <root@iccluster143.iccluster.epfl.ch>
2022-10-06 04:28:26 +0200

    simplify main running script

--- overgeneration.sh
+++ overgeneration.sh
@@ -1,32 +1,77 @@
-python gpt3_prompt.py --version a1 --start 0 --end 400 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 0 --end 400 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 400 --end 600 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 400 --end 600 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 600 --end 800 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 600 --end 800 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 800 --end 1000 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 800 --end 1000 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1000 --end 1400 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1000 --end 1400 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1400 --end 1600 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1400 --end 1600 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1600 --end 1800 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1600 --end 1800 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1800 --end 2000 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
-python gpt3_prompt.py --version a1 --start 1800 --end 2000 --prompt_type 0 --prompt_search --num_neighbors 4
-grep -o '"accept": true' ./data/output/entailment_contradiction_premise_a1.jsonl | wc -l
+# python run.py --start 14000 --end 16000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 16000 --end 18000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 18000 --end 20000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 20000 --end 22000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 22000 --end 24000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 24000 --end 26000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 26000 --end 28000 --type c2e --prompt_search --num_neighbors 8
+# python run.py --start 28000 --end 30000 --type c2e --prompt_search --num_neighbors 8
+
+
+# python run.py --start 14000 --end 16000 --type n2c --gen_type insertion
+# python run.py --start 16000 --end 18000 --type n2c --gen_type insertion
+# python run.py --start 18000 --end 20000 --type n2c --gen_type insertion
+# python run.py --start 20000 --end 22000 --type n2c --gen_type insertion
+# python run.py --start 22000 --end 24000 --type n2c --gen_type insertion
+# python run.py --start 24000 --end 26000 --type n2c --gen_type insertion
+# python run.py --start 26000 --end 28000 --type n2e --gen_type insertion
+python run.py --start 28000 --end 30000 --type n2c --gen_type insertion
+
+
+# python run.py --start 14000 --end 16000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 16000 --end 18000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 18000 --end 20000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 20000 --end 22000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 22000 --end 24000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 24000 --end 26000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 26000 --end 28000 --type e2c --prompt_search --num_neighbors 8
+# python run.py --start 28000 --end 30000 --type e2c --prompt_search --num_neighbors 8
+
+
+# python run.py --start 10000 --end 12000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 12000 --end 14000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 14000 --end 16000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 16000 --end 18000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 18000 --end 20000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 20000 --end 22000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 22000 --end 24000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 24000 --end 26000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 26000 --end 28000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 28000 --end 30000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 30000 --end 32000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 32000 --end 34000 --type e2n --prompt_search --num_neighbors 4
+# python run.py --start 34000 --end 36000 --type e2n --prompt_search --num_neighbors 4
+
+# python run.py --start 10000 --end 12000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 12000 --end 14000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 14000 --end 16000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 16000 --end 18000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 18000 --end 20000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 20000 --end 22000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 22000 --end 24000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 24000 --end 26000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 26000 --end 28000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 28000 --end 30000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 30000 --end 32000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 32000 --end 34000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 34000 --end 36000 --type c2n --prompt_search --num_neighbors 4
+# python run.py --start 36000 --end 38000 --type c2n --prompt_search --num_neighbors 4
+
+
+# python run.py --start 20000 --end 21000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 21000 --end 22000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 22000 --end 23000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 23000 --end 24000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 24000 --end 25000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 25000 --end 26000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 26000 --end 27000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 27000 --end 28000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 28000 --end 29000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 29000 --end 30000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+
+# python run.py --start 30000 --end 31000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 31000 --end 32000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 32000 --end 33000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 33000 --end 34000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 34000 --end 35000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
+# python run.py --start 35000 --end 36000 --type c2e --prompt_search --num_neighbors 4 --dataset anli
---
+++ run.py
@@ -0,0 +1,194 @@
+import json
+from pickle import FALSE
+import wandb
+import openai
+import redis
+import argparse
+import logging
+
+from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
+from torch.utils.data import DataLoader
+
+from cad_generator.base import (
+    cache_map,
+    type_map,
+    FilterDataset
+)
+
+from cad_generator.api import api_token, organization_token
+
+from cad_generator.generator import (
+    prompt_perturbation,
+    prompt_perturbation_insertion
+)
+
+from cad_generator.utils import write_jsonl
+from cad_generator.filter import (
+    NLIEnsembleFilter,
+    AutomaticHeuristicFilter,
+    PerplexityFilter,
+    collect_accepted
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+openai.organization = organization_token
+
+runner = wandb.init(
+    project="gpt3-counterfactual-generation",
+    entity="causal_scaffold",
+    name="gpt_generation_logger"
+)
+
+
+def get_cache(args):
+    if args.gen_type == "completion":
+        cache_idx = 0
+    else:
+        cache_idx = 1
+
+    logger.info(
+        f"Cache Index: {cache_map[args.dataset][args.type][cache_idx]}")
+    cache = redis.Redis(
+        host='localhost',
+        port=6379,
+        db=cache_map[args.dataset][args.type][cache_idx]
+    )
+
+    return cache
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', type=str, default="e2c",
+                        help="type of counterfactual: e2c | c2e | e2n | c2n")
+    parser.add_argument('--gen_type', type=str, default="completion",
+                        help="type of gpt-3 generation: completion | insertion")
+    parser.add_argument('--out_dir', type=str, default="./data/output",
+                        help="output directory")
+    parser.add_argument('--filter_dir', type=str, default="./data/filtered",
+                        help="filtered output directory")
+    parser.add_argument('--dataset', type=str, default="snli",
+                        help="base dataset for conterfactual generation")
+    parser.add_argument('--demo_dir', type=str, default="./data/examples",
+                        help="demonstration directory")
+    parser.add_argument('--start', type=int, default=0,
+                        help="start position of the dataset")
+    parser.add_argument('--end', type=int, default=100,
+                        help="end position of the dataset")
+    parser.add_argument('--api_idx', type=int, default=0,
+                        help="data record version")
+    parser.add_argument('--local_rank', type=str, default="0",
+                        help="locak rank of cuda devices")
+    parser.add_argument('--num_neighbors', type=int, default=8,
+                        help="number of nearest neighbors")
+    parser.add_argument('--embed_type', type=str, default="CLS",
+                        help="type of embedding method")
+    parser.add_argument('--metric', type=str, default="euclidean",
+                        help="distance metric used for nearest neighbor")
+    parser.add_argument('--encoder_name', type=str, default="roberta-large-nli-mean-tokens",
+                        help="name of the encoder for sentence embedding")
+    parser.add_argument('--prompt_search', action='store_true', default=False,
+                        help="enable dynamically selecting demonstrations")
+
+    args = parser.parse_args()
+    label_pair = type_map[args.type]
+    label = label_pair[0]
+    to_label = label_pair[1]
+
+    openai.api_key = api_token[args.api_idx]
+
+    args.lable = label
+    args.to_label = to_label
+    args.generate = False
+
+    if args.dataset == "snli":
+        args.data_pth = f"./data/input/{args.dataset}/{label}_spans.jsonl"
+    else:
+        args.data_pth = f"./data/input/{args.dataset}/{label}.jsonl"
+    args.demo_pth = f"{args.demo_dir}/{args.dataset}/{label}_{to_label}.jsonl"
+    args.out_pth = f"{args.out_dir}/{args.dataset}_{label}_{to_label}_{args.start}_{args.end}.jsonl"
+
+    if args.dataset == "anli":
+        args.mode = "hypothesis"
+    else:
+        args.mode = "premise"
+
+    cache = get_cache(args)
+
+    if args.prompt_search:
+        encoder = SentenceTransformer(args.encoder_name)
+    else:
+        encoder = None
+
+    logger.info("Collecting counterfactuals from GPT-3")
+
+    prompt_function = {
+        "insertion": prompt_perturbation_insertion,
+        "completion": prompt_perturbation
+    }
+
+    alien_counter_data = []
+    if args.generate:
+        counter_data = prompt_function[args.gen_type](args, cache, encoder)
+    else:
+        counter_data = []
+        for key in cache.keys():
+            record = json.loads(cache[key])
+            record[f"new_{args.mode}"] = record[args.mode].replace(
+                record["span_prev"], record["gen_out"]
+            )
+            if not record["accept"]:
+                if args.gen_type == "insertion" and "prompt" in record:
+                    alien_counter_data.append(record)
+                else:
+                    counter_data.append(record)
+
+    heuristic_filter = AutomaticHeuristicFilter()
+    ppl_filter = PerplexityFilter()
+    logger.info(
+        f"Filtering thorugh Heuristic and Perplexity Filter")
+
+    h_filtered = heuristic_filter.run(counter_data, cache, args.mode)
+    ppl_filtered = ppl_filter.run(h_filtered, cache, args.mode)
+
+    nli_filter = NLIEnsembleFilter(cache, args.mode, args.local_rank)
+    logger.info(
+        f"Initializing {len(nli_filter.hf_model_names)} filtering models")
+
+    counter_ds = FilterDataset(ppl_filtered)
+    loader = DataLoader(counter_ds, batch_size=32, shuffle=False)
+
+    logger.info("Filtering through NLI Ensemble Filter ...")
+    for i, batch in enumerate(tqdm(loader)):
+        filtered_batch = nli_filter.filter(batch, threshold=0.3)
+        if i > 0:
+            write_jsonl(filtered_batch, args.out_pth, mode="a")
+        else:
+            write_jsonl(filtered_batch, args.out_pth, mode="w")
+
+    artifact = wandb.Artifact(
+        f"{args.dataset}_{label}_{to_label}_{args.start}_{args.end}",
+        type='dataset'
+    )
+    # artifact.add_file(args.out_pth)
+    # runner.log_artifact(artifact)
+
+    accepted, rejected = collect_accepted(cache)
+    logger.info(f"Number of total accepted data: {len(accepted)}")
+    logger.info(f"Number of total rejected data: {len(rejected)}")
+
+    accepted_path = f"./data/filtered/{args.dataset}/{args.type}_{args.gen_type}.jsonl"
+    write_jsonl(accepted, accepted_path)
+    artifact = wandb.Artifact(
+        f"{args.dataset}_{args.type}_{args.gen_type}",
+        type='dataset'
+    )
+    artifact.add_file(accepted_path)
+    runner.log_artifact(artifact)
+
+    logger.info("Counterfactual collection complete.")
