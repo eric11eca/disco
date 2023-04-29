ed2d9346a6f5340f49615c528a8ab63390a19c5b
eric11eca <chenz16@rose-hulman.edu>
2023-04-29 02:13:25 +0200

    add sentence pair classification prompt template

---
+++ cad_generator/prompt/template/sentence_pair_classification.py
@@ -0,0 +1,179 @@
+from typing import List
+from dataclasses import dataclass
+
+from cad_generator.prompt.core import (
+    BasePrompt,
+    BaseExample,
+    BaseComposer,
+    BaseExampleReader,
+)
+
+
+@dataclass
+class SentencePairPrompt(BasePrompt):
+    guid: str
+    sentence1: str
+    sentence2: str
+    label: str
+    new_label: str
+    sentence1_spans: List[str]
+    sentence2_spans: List[str]
+    prompt: str
+    prefix: str
+    suffix: str
+    span_prev: str
+    gen_out: str
+    score: float
+    accept: bool
+
+    def __dict__(self) -> dict:
+        return {
+            "guid": self.guid,
+            "sentence1": self.sentence1,
+            "sentence2": self.sentence2,
+            "label": self.label,
+            "new_label": self.new_label,
+            "prompt": self.prompt,
+            "prefix": self.prefix,
+            "suffix": self.suffix,
+            "span_prev": self.span_prev,
+            "gen_out": self.gen_out,
+            "score": self.score,
+            "accept": self.accept,
+        }
+
+
+@dataclass
+class SentencePairExample(BaseExample):
+    guid: str
+    text_input: str
+    text_output: str
+
+    def __dict__(self) -> dict:
+        return {
+            "guid": self.guid,
+            "text_input": self.text_input,
+            "text_output": self.text_output,
+        }
+
+
+class SentencePairExampelReader(BaseExampleReader):
+    @ staticmethod
+    def _read(instance, args):
+        """Reads a single json line from the target file. Modify here when the json schema changes
+
+        :param instance: the instance to be read
+        :param args: the configuration arguments
+        :rtype instance: situation_modeling.readers.input_example.InputBase
+        """
+        guid = instance["guid"]
+        text_input = instance["input"]
+        text_output = instance["output"]
+        return SentencePairExample(
+            guid=guid,
+            text_input=text_input,
+            text_output=text_output
+        )
+
+
+class SentencePairComposer(BaseComposer):
+    @ staticmethod
+    def _build_promtp_instance(
+        template,
+        template_insert,
+        instance,
+        span, answer_choices,
+        span_idx,
+    ):
+        guid = instance["guid"]
+        sentence1 = instance["sentence1"]
+        sentence2 = instance["sentence2"]
+        label = instance["label"]
+        new_label = instance["new_label"]
+
+        prompt = SentencePairComposer._compose_prompt(
+            template,
+            {"sentence1": sentence1,
+             "sentence2": sentence2,
+             "span": span},
+        )
+        prompt_insert = SentencePairComposer._compose_prompt(
+            template_insert,
+            {"sentence1": sentence1,
+             "sentence2": sentence2,
+             "span": span,
+             "label": new_label,
+             "answer_choices": answer_choices}
+        )
+        prefix = prompt_insert.split("[insert]")[0]
+        suffix = prompt_insert.split("[insert]")[1]
+
+        prompt_instance = SentencePairPrompt(
+            guid=f"{guid}spanid={span_idx}",
+            sentence1=sentence1,
+            sentence2=sentence2,
+            label=label,
+            new_label=new_label,
+            prompt=prompt,
+            prefix=prefix,
+            suffix=suffix,
+            span_prev=span,
+            gen_out="",
+            score=0.0,
+            accept=False,
+        )
+        return prompt_instance
+
+    @ staticmethod
+    def _read(instance, templates, mode):
+        """Reads a single json line from the target file. Modify here when the json schema changes
+
+        :param instance: the instance to be read
+        :param templates: the templates to be used for prompt
+        :rtype problem: List[SentencePairPrompt]
+        """
+        sentence1_spans = list(set(instance["sentence1_spans"]))
+        sentence2_spans = list(set(instance["sentence2_spans"]))
+        problems = []
+
+        template = templates.template
+        template_insert = templates.template_insert
+        answer_choices = templates.answer_choices
+
+        if mode == "sentence1":
+            problems1 = [
+                SentencePairComposer._build_promtp_instance(
+                    template, template_insert, instance, span, answer_choices, i)
+                for i, span in enumerate(sentence1_spans)]
+            problems.extend(problems1)
+        elif mode == "sentence2":
+            problems2 = [
+                SentencePairComposer._build_promtp_instance(
+                    template, template_insert, instance, span, answer_choices, i)
+                for i, span in enumerate(sentence2_spans)]
+            problems.extend(problems2)
+        return problems
+
+    @classmethod
+    def read_and_compose(cls, instances, cache, templates):
+        """The method responsible for parsing in the input file. Implemented here
+        to make the overall pipeline more transparent.
+
+        :param instances: instances to be processed for perturbation
+        :param cache: the database instance
+        :param templates: the templates to be used for prompt
+        """
+        mode = templates.mode
+        seed_records = []
+
+        for instance in instances:
+            records = cls._read(instance, templates, mode)
+            seed_records.extend(records),
+
+        for record in seed_records:
+            record["accept"] = cls._commit(record, cache)
+
+        seed_records = [
+            record for record in seed_records if not record["accept"]]
+
+        return seed_records
--- run.py
+++ run.py
@@ -1,35 +1,23 @@
-import json
 import wandb
 import openai
-import redis
-import argparse
+import hydra
 import logging
 import transformers
 
 from tqdm import tqdm
-from sentence_transformers import SentenceTransformer
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
-from cad_generator.base import (
-    cache_map,
-    type_map,
-    FilterDataset
-)
-
-from cad_generator.api import api_token, organization_token
 from cad_generator.db import get_database
-
-from cad_generator.generator import (
-    prompt_perturbation,
-    prompt_perturbation_insertion
-)
-
 from cad_generator.utils import write_jsonl
+from cad_generator.prompt.core import Task
+from cad_generator.generator import Generator
 from cad_generator.filter import (
     NLIEnsembleFilter,
     AutomaticHeuristicFilter,
     PerplexityFilter,
-    collect_accepted
+    collect_accepted,
+    FilterDataset,
 )
 
 logging.basicConfig(
@@ -39,175 +27,38 @@ logger = logging.getLogger(__name__)
 
 transformers.logging.set_verbosity_error()
 
-openai.organization = organization_token
 
-runner = wandb.init(
-    project="gpt3-counterfactual-generation",
-    entity="causal_scaffold",
-    name="gpt_generation_logger"
-)
+class NeuroCrowdRunner:
+    def __init__(self, args, cache, task_container: Task):
+        self.args = args
+        self.task_container = task_container
+        self.generator = Generator(args, cache)
+        self.generation_outputs = []
+
+    def run_task_setup(self):
+        task = self.task_container.get_task()
+        task.run_setup()
 
 
-def get_cache(args):
-    if args.gen_type == "completion":
-        cache_idx = 0
-    else:
-        cache_idx = 1
+@hydra.main(config_path="config/secret/", config_name="keys")
+def set_up_api(keys: DictConfig):
+    openai.organization = keys.organization_token
+    openai.api_key = keys.api_token1
 
-    logger.info(
-        f"Cache Index: {cache_map[args.dataset][args.type][cache_idx]}")
-    cache = redis.Redis(
-        host='localhost',
-        port=6379,
-        db=cache_map[args.dataset][args.type][cache_idx]
+
+@hydra.main(config_path="config", config_name="config")
+def main(args: DictConfig):
+    runner = wandb.init(
+        project=args.project,
+        entity=args.entity,
+        name=args.name,
     )
-
-    return cache
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--type', type=str, default="e2c",
-                        help="type of counterfactual: e2c | c2e | e2n | c2n")
-    parser.add_argument('--gen_type', type=str, default="completion",
-                        help="type of gpt-3 generation: completion | insertion")
-    parser.add_argument('--out_dir', type=str, default="./data/output",
-                        help="output directory")
-    parser.add_argument('--filter_dir', type=str, default="./data/filtered",
-                        help="filtered output directory")
-    parser.add_argument('--dataset', type=str, default="snli_hard",
-                        help="base dataset for conterfactual generation")
-    parser.add_argument('--demo_dir', type=str, default="./data/examples",
-                        help="demonstration directory")
-    parser.add_argument('--start', type=int, default=0,
-                        help="start position of the dataset")
-    parser.add_argument('--end', type=int, default=100,
-                        help="end position of the dataset")
-    parser.add_argument('--api_idx', type=int, default=0,
-                        help="data record version")
-    parser.add_argument('--local_rank', type=str, default="0",
-                        help="locak rank of cuda devices")
-    parser.add_argument('--num_neighbors', type=int, default=8,
-                        help="number of nearest neighbors")
-    parser.add_argument('--embed_type', type=str, default="CLS",
-                        help="type of embedding method")
-    parser.add_argument('--metric', type=str, default="euclidean",
-                        help="distance metric used for nearest neighbor")
-    parser.add_argument('--encoder_name', type=str, default="roberta-large-nli-mean-tokens",
-                        help="name of the encoder for sentence embedding")
-    parser.add_argument('--prompt_search', action='store_true', default=False,
-                        help="enable dynamically selecting demonstrations")
-
-    args = parser.parse_args()
-    label_pair = type_map[args.type]
-    label = label_pair[0]
-    to_label = label_pair[1]
-
-    openai.api_key = api_token[args.api_idx]
-
-    args.lable = label
-    args.to_label = to_label
-    args.generate = True
-
-    if args.dataset == "snli":
-        args.data_pth = f"./data/input/{args.dataset}/{label}_spans.jsonl"
-    else:
-        args.data_pth = f"./data/input/{args.dataset}/{label}_spans.jsonl"
-    args.demo_pth = f"{args.demo_dir}/{args.dataset}/{label}_{to_label}.jsonl"
-    args.out_pth = f"{args.out_dir}/{args.dataset}_{label}_{to_label}_{args.start}_{args.end}.jsonl"
-
-    if args.dataset == "anli":
-        args.mode = "hypothesis"
-    else:
-        args.mode = "premise"
 
     cache = get_database(args.dataset, args.type)
 
-    if args.prompt_search:
-        encoder = SentenceTransformer(args.encoder_name)
-    else:
-        encoder = None
 
-    logger.info("Collecting counterfactuals from GPT-3")
-
-    prompt_function = {
-        "insertion": prompt_perturbation_insertion,
-        "completion": prompt_perturbation
-    }
-
-    alien_counter_data = []
-    if args.generate:
-        if args.type == "n2e":
-            counter_data = prompt_function["insertion"](args, cache, encoder)
-        else:
-            counter_data = prompt_function[args.gen_type](args, cache, encoder)
-
-    else:
-        counter_data = []
-        cursor = list(cache.find({}))
-        for record in tqdm(cursor):
-            # if record["accept"]:
-            record[f"new_{args.mode}"] = record[args.mode].replace(
-                record["span_prev"], record["gen_out"]
-            )
-            record["score"] = 0.0
-
-            if args.gen_type == "insertion" and "prompt" in record:
-                alien_counter_data.append(record)
-            else:
-                counter_data.append(record)
-
-    logger.info(f"Filtering {len(counter_data)} counterfactuals")
-
-    heuristic_filter = AutomaticHeuristicFilter()
-    ppl_filter = PerplexityFilter()
-    nli_filter = NLIEnsembleFilter(cache, args.mode, args.local_rank)
-    logger.info(
-        f"Initialized {len(nli_filter.hf_model_names)} filtering models")
-
-    logger.info(f"Filtering thorugh Heuristic Filter ...")
-    h_filtered = heuristic_filter.run(counter_data, cache, args.mode)
-
-    counter_ds = FilterDataset(h_filtered)
-    loader = DataLoader(counter_ds, batch_size=32, shuffle=False)
-
-    logger.info("Filtering through NLI Ensemble Filter ...")
-    filtered_batch = []
-    for i, batch in enumerate(tqdm(loader)):
-        filtered_batch += nli_filter.filter(batch, threshold=0.3)
-
-    write_jsonl(filtered_batch, args.out_pth, mode="w")
-
-    accepted_batch = [
-        record for record in filtered_batch if record["accept"]]
-    logger.info(
-        f"NLI filter accepted {nli_filter.global_counter} counterfactuals")
-
-    # logger.info("Filtering through Perplexity Filter ...")
-    # p_filtered = ppl_filter.run(
-    #     accepted_batch, cache, args.mode, threshold=150)
-    # logger.info(f"PPL filter accepted {len(p_filtered)} counterfactuals")
-
-    if args.generate:
-        write_jsonl(accepted_batch, args.out_pth)
-        artifact = wandb.Artifact(
-            f"{args.dataset}_{label}_{to_label}_{args.start}_{args.end}",
-            type='dataset'
-        )
-        artifact.add_file(args.out_pth)
-        runner.log_artifact(artifact)
-
-    accepted, rejected = collect_accepted(cache)
-    logger.info(f"Number of total accepted data: {len(accepted)}")
-    logger.info(f"Number of total rejected data: {len(rejected)}")
-
-    accepted_path = f"./data/filtered/{args.dataset}/{args.type}_{args.gen_type}.jsonl"
-    write_jsonl(accepted, accepted_path, mode="a")
-    artifact = wandb.Artifact(
-        f"{args.dataset}_{args.type}_{args.gen_type}",
-        type='dataset'
-    )
-    artifact.add_file(accepted_path)
-    runner.log_artifact(artifact)
+if __name__ == "__main__":
+    set_up_api()
+    main()
 
     logger.info("Counterfactual collection complete.")
