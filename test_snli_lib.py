9f1b68119d7515af374ec9d3f16cb223a6e9900f
Zeming Chen <zeming.chen@epfl.ch>
2023-04-30 20:03:29 +0200

    test generator and postprocess

--- test_snli_lib.py
+++ test_snli_lib.py
@@ -1,6 +1,10 @@
-from omegaconf import OmegaConf
+import hydra
+import openai
+
+from omegaconf import OmegaConf, DictConfig
 from hydra import initialize
 
+from distiller.generator import Generator
 from distiller.db import get_database
 from distiller.prompt.lib.snli import SNLITask
 
@@ -9,18 +13,28 @@ cache = get_database(
     type="masked_cad_premise"
 )
 
-initialize(config_path="./templates", version_base="1.3")
-
 args = OmegaConf.create({
-    'label': 'entailment',
+    'model_name': 'gpt-3-003',
+    'gen_type': 'completion',
+    'target_label': 'entailment',
     'template_name': 'masked_cad_premise',
     'data_pth': 'data/snli/input/contradiction.jsonl',
     'demo_pth': 'data/snli/examples/contradiction_entailment.jsonl',
 })
 
-def main():
-    SNLITask.build_prompts(args, cache)
+@hydra.main(config_path="./config/secret/", config_name="keys")
+def set_up_api(keys: DictConfig):
+    openai.organization = keys.organization_token
+    openai.api_key = keys.api_token
 
+def main():
+    set_up_api()
+
+    initialize(config_path="./templates", version_base="1.3")
+    querys = SNLITask.build_prompts(args, cache)
+    generator = Generator(args, cache)
+    generation_outputs = generator.batch_generate(querys)
+    SNLITask.postprocess_generation(cache, generation_outputs)
 
 if __name__ == "__main__":
     main()
